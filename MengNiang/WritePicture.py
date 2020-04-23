9e42a8a2f36a27a11317927bb230d63a42af52c9
songzhouzhang <songzhouzhang@gmail.com>
2020-04-24 01:38:05 +0800

    1:增加数据保存

--- MengNiang/WritePicture.py
+++ MengNiang/WritePicture.py
@@ -10,7 +10,17 @@ def read_info_html():
 
 def read_json():
     print(cache_json_path)
-    return
+
+    file_r = open(cache_json_path, 'r+')
+    file_content = file_r.read()
+    file_r.close()
+    item_list = json.loads(file_content)
+    for item in item_list:
+        print(item['index'])
+        print(item['date'])
+        print(item['title'])
+        print(item['url'])
+        break
 
 
 read_json()
