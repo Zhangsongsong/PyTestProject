89e37476643d8fcef545b4f9082034061409adcb
zhangsongzhou <zhangsongzhou@inke.cn>
2023-09-05 19:10:47 +0800

    1：增加代码 + 2

--- MHQWE/Main.py
+++ MHQWE/Main.py
@@ -1,12 +1,14 @@
 import bs4 as BeautifulSoup4
 import requests
 
-import ssl
+import urllib3
 
-ssl_context = ssl.create_default_context()
+urllib3.disable_warnings()
 
 home_url = 'https://www.mhqwe.xyz.com'
 test_url = 'https://www.baidu.com'
+
+test_url_1 = 'https://www.mhqwe.xyz/play?linkId=2082973&bookId=1905&path=6&key=bNMFOi7DIVMv8kk7C8bz0A=='
 
 
 def get_home_pager(url):
@@ -14,7 +16,9 @@ def get_home_pager(url):
     if req_result.status_code == 200:
         html_str = req_result.content.decode("utf-8")
         soup = BeautifulSoup4.BeautifulSoup(html_str, 'html.parser')
-        print(soup)
+        # print(soup)
+        img_list = soup.select("#imgList")
+        print(img_list)
 
 
-get_home_pager(home_url)
+get_home_pager(test_url_1)
