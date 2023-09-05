ef0b3281319c4019fa710205aa8ee3df3801396e
zhangsongzhou <zhangsongzhou@inke.cn>
2023-09-05 17:22:45 +0800

    1：增加代码 + 1

--- MHQWE/Main.py
+++ MHQWE/Main.py
@@ -1,17 +1,20 @@
 import bs4 as BeautifulSoup4
-import requests as requests
+import requests
 
+import ssl
+
+ssl_context = ssl.create_default_context()
 
 home_url = 'https://www.mhqwe.xyz.com'
 test_url = 'https://www.baidu.com'
 
 
 def get_home_pager(url):
-    req_result = requests.get(url)
+    req_result = requests.get(url, verify=False)
     if req_result.status_code == 200:
         html_str = req_result.content.decode("utf-8")
-        soup = BeautifulSoup4(html_str,'html.parser')
+        soup = BeautifulSoup4.BeautifulSoup(html_str, 'html.parser')
         print(soup)
 
 
-get_home_pager(test_url)
+get_home_pager(home_url)
